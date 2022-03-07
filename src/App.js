d36a41b3ba6be0ea065953846586562b7d01fba0
Eliasari <elia21196@gmail.com>
2022-03-07 15:04:01 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,124 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div class="container">
+      <div class="card col-sm-4">
+        <div class="card-header bg-danger text-white">
+          <h4>Konversi Bilangan Biner</h4>
+        </div>
+        <div class="card-body">
+          Biner
+          <input type="number" id="bilangan" class="form-control" />
+
+          <label for="pilihanConversi">Pilihan Konversi</label>
+          <select id="PilihanConversi" class="form-control">
+            <option value="Biner">Biner</option>
+            <option value="Oktal">Oktal</option>
+            <option value="Desimal">Desimal</option>
+            <option value="Heksadesimal">Heksadesimal</option>
+          </select>
+
+          <br></br>
+          <button type="button" class="btn btn-danger btn-block"
+          onclick="count()">
+            Convert
+          </button>
+          <br></br>
+          <br></br>
+          Hasil
+          <input type="text" id="hasil" class="form-control" readonly />
+        </div>
+      </div>
+
+      <br></br>
+      <div class="card col-sm-4">
+        <div class="card-header bg-danger text-white">
+          <h4>Konversi Bilangan Oktal</h4>
+        </div>
+        <div class="card-body">
+          Oktal
+          <input type="number" id="bilangan" class="form-control" />
+
+          <label for="pilihanConversi">Pilihan Konversi</label>
+          <select id="PilihanConversi" class="form-control">
+            <option value="Biner">Biner</option>
+            <option value="Oktal">Oktal</option>
+            <option value="Desimal">Desimal</option>
+            <option value="Heksadesimal">Heksadesimal</option>
+          </select>
+
+          <br></br>
+          <button type="button" class="btn btn-danger btn-block"
+          onclick="count()">
+            Convert
+          </button>
+          <br></br>
+          <br></br>
+          Hasil
+          <input type="text" id="hasil" class="form-control" readonly />
+        </div>
+      </div>
+
+    <br></br>
+    <div class="card col-sm-4">
+        <div class="card-header bg-danger text-white">
+          <h4>Konversi Bilangan Desimal</h4>
+        </div>
+        <div class="card-body">
+          Desimal
+          <input type="number" id="bilangan" class="form-control" />
+
+          <label for="pilihanConversi">Pilihan Konversi</label>
+          <select id="PilihanConversi" class="form-control">
+            <option value="Biner">Biner</option>
+            <option value="Oktal">Oktal</option>
+            <option value="Desimal">Desimal</option>
+            <option value="Heksadesimal">Heksadesimal</option>
+          </select>
+
+          <br></br>
+          <button type="button" class="btn btn-danger btn-block"
+          onclick="count()">
+            Convert
+          </button>
+          <br></br>
+          <br></br>
+          Hasil
+          <input type="text" id="hasil" class="form-control" readonly />
+        </div>
+      </div>
+
+      <br></br>
+      <div class="card col-sm-4">
+        <div class="card-header bg-danger text-white">
+          <h4>Konversi Bilangan Hexadesimal</h4>
+        </div>
+        <div class="card-body">
+          Hexadesimal
+          <input type="number" id="bilangan" class="form-control" />
+
+          <label for="pilihanConversi">Pilihan Konversi</label>
+          <select id="PilihanConversi" class="form-control">
+            <option value="Biner">Biner</option>
+            <option value="Oktal">Oktal</option>
+            <option value="Desimal">Desimal</option>
+            <option value="Heksadesimal">Heksadesimal</option>
+          </select>
+
+          <br></br>
+          <button type="button" class="btn btn-danger btn-block"
+          onclick="count()">
+            Convert
+          </button>
+          <br></br>
+          <br></br>
+          Hasil
+          <input type="text" id="hasil" class="form-control" readonly />
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
